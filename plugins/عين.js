0e0755a1777ac586c47bad7dba4566e243aae8d5
ayanoo01 <mono1122334455666@gmail.com>
2024-07-02 13:31:42 +0300

    Update عين.js

--- plugins/عين.js
+++ plugins/عين.js
@@ -10,9 +10,15 @@ let handler = async (m, { conn, command, usedPrefix }) => {
     let src = await (await fetch('https://gist.githubusercontent.com/Kyutaka101/4e01c190b7d67225ad7a86d388eeedf6/raw/67f0de059cea4b965a3f3bf211c12fc9c48043e5/gistfile1.txt')).json()
   let json = src[Math.floor(Math.random() * src.length)]
     let caption = `*${command.toUpperCase()}*
-  ❐↞┇الـوقـت⏳↞ *${(timeout / 1000).toFixed(2)} ┇
-  *استخدم .انسحب للأنسحاب*
-  ❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇
+  ┓──⏣──━━──⏣──┏
+*_❍ ↚┇الـــوقــت ⏱️_*
+> ${(timeout / 1000).toFixed(2)
+ 
+
+*_❍ ↚┇الـــجــائــزة_*
+> ${poin} 
+
+┛──⏣──━━──⏣──
 『𝚃𝚛𝚊𝚏𝚊𝚕𝚐𝚊𝚛 𝙻𝚊𝚘-𝚋𝚘𝚝 』
      `.trim()
     conn.tebakbendera[id] = [
