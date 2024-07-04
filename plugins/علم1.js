fd44fd244e27518f858c84e77677b7af9b3dd377
ayanoo01 <mono1122334455666@gmail.com>
2024-07-04 23:06:36 +0300

    Update علم1.js

--- plugins/علم1.js
+++ plugins/علم1.js
@@ -7,25 +7,29 @@ let handler = async (m, { conn, command, usedPrefix }) => {
         conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tebakbendera[id][0])
         throw false
     }
-    let src = await (await fetch('https://raw.githubusercontent.com/mikey44dd/hanry-bot/master/src/JSON/%D8%B9%D9%84%D9%85.json')).json()
-  let json = src[Math.floor(Math.random() * src.length)]
-    let caption = `*${command.toUpperCase()}*
-  *❐↞┇الـوقـت⏳↞${(timeout / 1000).toFixed(2)} ┇*
-  *استخدم .انسحب للأنسحاب*
-  ❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇
-*❰BY :𝚃𝚛𝚊𝚏𝚊𝚕𝚐𝚊𝚛 𝙻𝚊𝚘 🧧❱*
-     `.trim()
+    let src = await (await fetch('https://raw.githubusercontent.com/Khaledst1/-/main/Bot-AlSoltan1.json')).json()
+    let json = src[Math.floor(Math.random() * src.length)]
+    let caption = `*· • • ━━ ⌝☘️⌞ ━━ • • ·*
+*${command.toUpperCase()}*
+*🜋↫╎السـؤال ✍🏻⇜『ما اسم الدولة التي يمثلها هذا العلم؟』*
+*🜋↫╎الـوقـت⏳↞ ${(timeout / 1000).toFixed(2)} ثواني ┇*
+*استخدم .انسحب للأنسحاب*
+*🜋↫╎الـجـائزة💰↞ ${poin} نقاط┇*
+∞┇━━━ •☘️• ━━━┇∞
+*✠ ~تــ✍︎ــوقــيــع ↯:~*
+*『𝚃𝚛𝚊𝚏𝚊𝚕𝚐𝚊𝚛 𝙻𝚊𝚘』*
+    `.trim()
     conn.tebakbendera[id] = [
-        await conn.sendFile(m.chat, json.question, '', caption, m),
+        await conn.sendFile(m.chat, json.img, '', caption, m),
         json, poin,
         setTimeout(() => {
-            if (conn.tebakbendera[id]) conn.reply(m.chat, `❮ ⌛┇انتهي الوقت┇⌛❯\n❐↞┇الاجـابـة✅↞ ${json.response}*┇`, conn.tebakbendera[id][0])
+            if (conn.tebakbendera[id]) conn.reply(m.chat, `❮ ⌛┇انتهي الوقت┇⌛❯\n❐↞┇الاجـابـة✅↞ *${json.name}* ┇`, conn.tebakbendera[id][0])
             delete conn.tebakbendera[id]
         }, timeout)
     ]
 }
-handler.help = ['guessflag']
-handler.tags = ['game']
+handler.help = ['علم']
+handler.tags = ['fun']
 handler.command = /^علم/i
 
 export default handler
