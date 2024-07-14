8ab83e3d9b8cb1d08f8da10d6c037999052c6d81
ayanoo01 <mono1122334455666@gmail.com>
2024-07-14 15:30:52 +0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -1005,7 +1005,7 @@ export async function handler(chatUpdate) {
           autoread2: false,
           restrict: false,
           antiCall: false,
-          antiPrivate: false,
+          antiPrivate: true,
 	  modejadibot: true,
           antispam: false,
 	  audios_bot: true,
