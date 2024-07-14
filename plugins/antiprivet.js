fb099650bbd387eb6b10944276a2af423c3ac0a4
ayanoo01 <mono1122334455666@gmail.com>
2024-07-14 22:07:18 +0300

    Update antiprivet.js

--- plugins/antiprivet.js
+++ plugins/antiprivet.js
@@ -1,9 +1,4 @@
 export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
-    const datas = global
-    const idioma = datas.db.data.users[m.sender].language
-    const _translate = JSON.parse(fs.readFileSync(./language/ar.json))
-    const tradutor = _translate.plugins._antiprivado
-
   if (m.isBaileys && m.fromMe) return !0;
   if (m.isGroup) return !1;
   if (!m.message) return !0;
@@ -11,7 +6,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[this.user.jid] || {};
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(tradutor.texto1, false, {mentions: [m.sender]});
+    await m.reply(*[😊] اهلا يا @${m.sender.split@[0]}, ممنوع التحدث مع البوت في الدردشه الخاص و الان سيتم حظرك\nراسل المطور لكي يدخل لك البوت في جروبك\nرقمه: +994404282709*, false, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
   return !1;
